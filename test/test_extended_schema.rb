997504e889a368579ab98dad70513d6f282ee9e6
Kenny Hoxworth <hoxworth@gmail.com>
2011-03-08 20:46:40 -0500

    I apparenntly included the extended schema stuff with the last commit. Well, here is the start of testing for that.

---
+++ test/test_extended_schema.rb
@@ -0,0 +1,68 @@
+require 'test/unit'
+require File.dirname(__FILE__) + '/../lib/json-schema'
+
+class BitwiseAndAttribute < JSON::Schema::Attribute
+  def self.validate(current_schema, data, fragments, validator, options = {})
+    if data.is_a?(Integer) && data & current_schema.schema['bitwise-and'].to_i == 0
+      message = "The property '#{build_fragment(fragments)}' did not evaluate  to true when bitwise-AND'd with  #{current_schema.schema['bitwise-or']}"
+      raise JSON::Schema::ValidationError.new(message, fragments, current_schema)
+    end
+  end
+end
+
+class ExtendedSchema < JSON::Schema::Validator
+  def initialize
+    super
+    extend_schema_definition("http://json-schema.org/draft-03/schema#")
+    @attributes["bitwise-and"] = BitwiseAndAttribute
+    @uri = URI.parse("http://test.com/test.json")
+  end
+  
+  JSON::Validator.register_validator(self.new)
+end
+
+class JSONSchemaTestExtendedSchema < Test::Unit::TestCase
+  def test_schema_from_file
+    schema = {
+      "$schema" => "http://json-schema.org/draft-03/schema#",
+      "properties" => {
+        "a" => {
+          "bitwise-and" => 1
+        },
+        "b" => {
+          "type" => "string"
+        }
+      }
+    }
+    
+    data = {"a" => 0, "b" => "taco"}
+    assert(JSON::Validator.validate(schema,data))
+    data = {"a" => 1, "b" => "taco"}
+    assert(JSON::Validator.validate(schema,data))
+    data = {"a" => 1, "b" => 5}
+    assert(!JSON::Validator.validate(schema,data))
+    
+    schema = {
+      "$schema" => "http://test.com/test.json",
+      "properties" => {
+        "a" => {
+          "bitwise-and" => 1
+        },
+        "b" => {
+          "type" => "string"
+        }
+      }
+    }
+    
+    data = {
+      "a" => 0
+    }
+    
+    data = {"a" => 1, "b" => "taco"}
+    assert(JSON::Validator.validate(schema,data))
+    data = {"a" => 0, "b" => "taco"}
+    assert(!JSON::Validator.validate(schema,data))
+    data = {"a" => 1, "b" => 5}
+    assert(!JSON::Validator.validate(schema,data))
+  end
+end
