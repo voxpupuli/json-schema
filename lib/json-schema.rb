ec50ee9f9512ecd796dcf1e726426fc76a53eafc
Kenny Hoxworth <hoxworth@gmail.com>
2011-03-18 21:55:14 -0400

    Yikes, might want to include that UUID def!

--- lib/json-schema.rb
+++ lib/json-schema.rb
@@ -6,3 +6,4 @@ require 'validator'
 Dir[File.join(File.dirname(__FILE__), "json-schema/attributes/*")].each {|file| require file }
 Dir[File.join(File.dirname(__FILE__), "json-schema/validators/*")].each {|file| require file }
 require 'uri/file'
+require 'uri/uuid'
