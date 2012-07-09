a5d4a3428f9f02a417147c11dc21ec5060db3e8c
David Barri <japgolly@gmail.com>
2012-07-09 18:31:54 +1000

    Fixed tests. Extends and additionalproperties now work together.

--- lib/json-schema/attributes/ref.rb
+++ lib/json-schema/attributes/ref.rb
@@ -1,12 +1,28 @@
 module JSON
   class Schema
     class RefAttribute < Attribute
-      def self.validate(current_schema, data, fragments, validator, options = {})        
-        temp_uri = URI.parse(current_schema.schema['$ref'])
+      def self.validate(current_schema, data, fragments, validator, options = {})
+        uri,schema = get_referenced_uri_and_schema(current_schema.schema, current_schema, validator)
+
+        if schema
+          schema.validate(data, fragments, options)
+        elsif uri
+          message = "The referenced schema '#{uri.to_s}' cannot be found"
+          validation_error(message, fragments, current_schema, self, options[:record_errors])
+        else
+          message = "The property '#{build_fragment(fragments)}' was not a valid schema"
+          validation_error(message, fragments, current_schema, self, options[:record_errors])
+        end
+      end
+
+      def self.get_referenced_uri_and_schema(s, current_schema, validator)
+        uri,schema = nil,nil
+
+        temp_uri = URI.parse(s['$ref'])
         if temp_uri.relative?
           temp_uri = current_schema.uri.clone
           # Check for absolute path
-          path = current_schema.schema['$ref'].split("#")[0]
+          path = s['$ref'].split("#")[0]
           if path.nil? || path == ''
             temp_uri.path = current_schema.uri.path
           elsif path[0,1] == "/"
@@ -14,7 +30,7 @@ module JSON
           else
             temp_uri = current_schema.uri.merge(path)
           end
-          temp_uri.fragment = current_schema.schema['$ref'].split("#")[1]
+          temp_uri.fragment = s['$ref'].split("#")[1]
         end
         temp_uri.fragment = "" if temp_uri.fragment.nil?
 
@@ -43,12 +59,11 @@ module JSON
           end
 
           # We have the schema finally, build it and validate!
+          uri = temp_uri
           schema = JSON::Schema.new(target_schema,temp_uri,validator)
-          schema.validate(data, fragments, options)
-        else
-          message = "The referenced schema '#{temp_uri.to_s}' cannot be found"
-          validation_error(message, fragments, current_schema, self, options[:record_errors])
         end
+
+        [uri,schema]
       end
     end
   end
