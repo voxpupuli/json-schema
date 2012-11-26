f4d0e1dcaf45eae3952a005ec9b6f07ec0b3a163
Brian Hoffman <brianjhoffman@gmail.com>
2012-11-26 12:39:16 -0500

    added value to pattern att message in the style of bdb82ba

--- lib/json-schema/attributes/pattern.rb
+++ lib/json-schema/attributes/pattern.rb
@@ -5,7 +5,7 @@ module JSON
         if data.is_a?(String)
           r = Regexp.new(current_schema.schema['pattern'])
           if (r.match(data)).nil?
-            message = "The property '#{build_fragment(fragments)}' did not match the regex '#{current_schema.schema['pattern']}'"
+            message = "The property '#{build_fragment(fragments)}' value #{data.inspect} did not match the regex '#{current_schema.schema['pattern']}'"
             validation_error(message, fragments, current_schema, self, options[:record_errors])
           end
         end
