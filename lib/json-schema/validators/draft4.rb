59543ef50b379b556cc5b275e69d4bd803b6a718
Kenny <hoxworth@gmail.com>
2013-06-09 13:46:37 -0700

    Draft 4 validator

---
+++ lib/json-schema/validators/draft4.rb
@@ -0,0 +1,42 @@
+module JSON
+  class Schema
+
+    class Draft4 < Validator
+      def initialize
+        super
+        @attributes = {
+          "type" => JSON::Schema::TypeV4Attribute,
+          "allOf" => JSON::Schema::AllOfAttribute,
+          "anyOf" => JSON::Schema::AnyOfAttribute,
+          "oneOf" => JSON::Schema::OneOfAttribute,
+          "not" => JSON::Schema::NotAttribute,
+          "disallow" => JSON::Schema::DisallowAttribute,
+          "format" => JSON::Schema::FormatAttribute,
+          "maximum" => JSON::Schema::MaximumAttribute,
+          "minimum" => JSON::Schema::MinimumAttribute,
+          "minItems" => JSON::Schema::MinItemsAttribute,
+          "maxItems" => JSON::Schema::MaxItemsAttribute,
+          "uniqueItems" => JSON::Schema::UniqueItemsAttribute,
+          "minLength" => JSON::Schema::MinLengthAttribute,
+          "maxLength" => JSON::Schema::MaxLengthAttribute,
+          "multipleOf" => JSON::Schema::DivisibleByAttribute,
+          "enum" => JSON::Schema::EnumAttribute,
+          "properties" => JSON::Schema::PropertiesAttribute,
+          "pattern" => JSON::Schema::PatternAttribute,
+          "patternProperties" => JSON::Schema::PatternPropertiesAttribute,
+          "additionalProperties" => JSON::Schema::AdditionalPropertiesAttribute,
+          "items" => JSON::Schema::ItemsAttribute,
+          "additionalItems" => JSON::Schema::AdditionalItemsAttribute,
+          "dependencies" => JSON::Schema::DependenciesV4Attribute,
+          "extends" => JSON::Schema::ExtendsAttribute,
+          "$ref" => JSON::Schema::RefAttribute
+        }
+        @uri = URI.parse("http://json-schema.org/draft-04/schema#")
+      end
+
+      JSON::Validator.register_validator(self.new)
+      JSON::Validator.register_default_validator(self.new)
+    end
+
+  end
+end
