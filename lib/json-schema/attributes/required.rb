91685846232e86d939ecebb0c030af8060baa3a3
Kenny <hoxworth@gmail.com>
2013-06-23 13:36:45 -0700

    Adding in "default" stuff. Yay tests pass, now to add tests for new stuff.

--- lib/json-schema/attributes/required.rb
+++ lib/json-schema/attributes/required.rb
@@ -5,8 +5,15 @@ module JSON
         if data.is_a?(Hash)
           current_schema.schema['required'].each do |property,property_schema|
             if !data.has_key?(property)
-              message = "The property '#{build_fragment(fragments)}' did not contain a required property of '#{property}'"
-              validation_error(processor, message, fragments, current_schema, self, options[:record_errors])
+              prop_defaults = options[:insert_defaults] &&
+                              current_schema.schema['properties'] &&
+                              current_schema.schema['properties'][property] &&
+                              !current_schema.schema['properties'][property]["default"].nil? &&
+                              !current_schema.schema['properties'][property]["readonly"]
+              if !prop_defaults
+                message = "The property '#{build_fragment(fragments)}' did not contain a required property of '#{property}'"
+                validation_error(processor, message, fragments, current_schema, self, options[:record_errors])
+              end
             end
           end
         end
