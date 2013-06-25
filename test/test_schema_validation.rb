4f0ac2baceeb90494bbdafe8f5863c289453e46d
Kenny <hoxworth@gmail.com>
2013-06-25 11:29:02 -0700

    Testing for v3/v4 schema validation

--- test/test_schema_validation.rb
+++ test/test_schema_validation.rb
@@ -2,7 +2,7 @@ require 'test/unit'
 require File.dirname(__FILE__) + '/../lib/json-schema'
 
 class JSONSchemaValidation < Test::Unit::TestCase
-  def valid_schema
+  def valid_schema_v3
     {
       "$schema" => "http://json-schema.org/draft-03/schema#",
       "type" => "object",
@@ -14,7 +14,7 @@ class JSONSchemaValidation < Test::Unit::TestCase
     }
   end
 
-  def invalid_schema
+  def invalid_schema_v3
     {
       "$schema" => "http://json-schema.org/draft-03/schema#",
       "type" => "object",
@@ -26,17 +26,53 @@ class JSONSchemaValidation < Test::Unit::TestCase
     }
   end
 
-  def test_draft03_validation
-    data = {"b" => {"a" => 5}}
-    assert(JSON::Validator.validate(valid_schema,data,:validate_schema => true))
-    assert(!JSON::Validator.validate(invalid_schema,data,:validate_schema => true))
+  def valid_schema_v4
+    {
+      "$schema" => "http://json-schema.org/draft-04/schema#",
+      "type" => "object",
+      "required" => ["b"],
+      "properties" => {
+      }
+    }
   end
 
-  def test_validate_just_schema
-    errors = JSON::Validator.fully_validate_schema(valid_schema)
+  def invalid_schema_v4
+    {
+      "$schema" => "http://json-schema.org/draft-04/schema#",
+      "type" => "object",
+      "required" => "b",
+      "properties" => {
+      }
+    }
+  end
+
+  def test_draft03_validation
+    data = {"b" => {"a" => 5}}
+    assert(JSON::Validator.validate(valid_schema_v3,data,:validate_schema => true, :version => :draft3))
+    assert(!JSON::Validator.validate(invalid_schema_v3,data,:validate_schema => true, :version => :draft3))
+  end
+
+  def test_validate_just_schema_draft03
+    errors = JSON::Validator.fully_validate_schema(valid_schema_v3, :version => :draft3)
     assert_equal [], errors
 
-    errors = JSON::Validator.fully_validate_schema(invalid_schema)
+    errors = JSON::Validator.fully_validate_schema(invalid_schema_v3, :version => :draft3)
+    assert_equal 1, errors.size
+    assert_match /the property .*required.*did not match/i, errors.first
+  end
+
+
+  def test_draft04_validation
+    data = {"b" => {"a" => 5}}
+    assert(JSON::Validator.validate(valid_schema_v4,data,:validate_schema => true, :version => :draft4))
+    assert(!JSON::Validator.validate(invalid_schema_v4,data,:validate_schema => true, :version => :draft4))
+  end
+
+  def test_validate_just_schema_draft04
+    errors = JSON::Validator.fully_validate_schema(valid_schema_v4, :version => :draft4)
+    assert_equal [], errors
+
+    errors = JSON::Validator.fully_validate_schema(invalid_schema_v4, :version => :draft4)
     assert_equal 1, errors.size
     assert_match /the property .*required.*did not match/i, errors.first
   end
