0c3746150eb3818ee6b441175c8b04c429ee4369
Kenny <hoxworth@gmail.com>
2013-07-02 22:03:59 -0700

    Adding invalid fragment assertions

--- test/schemas/test_fragment_resolution.rb
+++ test/schemas/test_fragment_resolution.rb
@@ -1,4 +1,5 @@
 require 'test/unit'
+require File.dirname(__FILE__) + '/../lib/json-schema'
 
 class FragmentResolution < Test::Unit::TestCase
   def test_fragment_resolution
@@ -17,5 +18,13 @@ class FragmentResolution < Test::Unit::TestCase
     data = {"b" => 5}
     assert(!JSON::Validator.validate(schema,data))
     assert(JSON::Validator.validate(schema,data,:fragment => "#/properties/a"))
+
+    assert_raise JSON::Schema::SchemaError do
+      JSON::Validator.validate!(schema,data,:fragment => "/properties/a")
+    end
+
+    assert_raise JSON::Schema::SchemaError do
+      JSON::Validator.validate!(schema,data,:fragment => "#/properties/b")
+    end
   end
 end
