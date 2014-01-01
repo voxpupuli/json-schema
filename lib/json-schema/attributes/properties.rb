850e4832e4a4746f35eb960df5c2a6f27194a430
Kenny Hoxworth <hoxworth@gmail.com>
2013-12-31 22:00:27 -0800

    Fix symbol keys in data

--- lib/json-schema/attributes/properties.rb
+++ lib/json-schema/attributes/properties.rb
@@ -10,18 +10,18 @@ module JSON
                !property_schema['readonly'] &&
                options[:insert_defaults]
               default = property_schema['default']
-              data[property] = (default.is_a?(Hash) ? default.clone : default)
+              data[property.to_s] = (default.is_a?(Hash) ? default.clone : default)
             end
 
-            if property_schema['required'] && !data.has_key?(property.to_s)
+            if property_schema['required'] && !data.has_key?(property.to_s) && !data.has_key?(property.to_sym)
               message = "The property '#{build_fragment(fragments)}' did not contain a required property of '#{property}'"
               validation_error(processor, message, fragments, current_schema, self, options[:record_errors])
             end
 
             if data.has_key?(property.to_s) || data.has_key?(property.to_sym)
               schema = JSON::Schema.new(property_schema,current_schema.uri,validator)
-              fragments << property
-              schema.validate(data[property],fragments,processor,options)
+              fragments << property.to_s
+              schema.validate(data[property.to_s],fragments,processor,options)
               fragments.pop
             end
           end
--- lib/json-schema/attributes/properties_v4.rb
+++ lib/json-schema/attributes/properties_v4.rb
@@ -10,13 +10,13 @@ module JSON
                !property_schema['readonly'] &&
                options[:insert_defaults]
               default = property_schema['default']
-              data[property] = (default.is_a?(Hash) ? default.clone : default)
+              data[property.to_s] = (default.is_a?(Hash) ? default.clone : default)
             end
 
-            if data.has_key?(property.to_s)
+            if data.has_key?(property.to_s) || data.has_key?(property.to_sym)
               schema = JSON::Schema.new(property_schema,current_schema.uri,validator)
-              fragments << property
-              schema.validate(data[property],fragments,processor,options)
+              fragments << property.to_s
+              schema.validate(data[property.to_s],fragments,processor,options)
               fragments.pop
             end
           end
