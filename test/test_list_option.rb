dcb0be525e05b4fe47c6eb4167c67cbc9bfce555
Kenny Hoxworth <khoxworth@twilio.com>
2014-07-19 14:13:25 -0700

    Add list option test

---
+++ test/test_list_option.rb
@@ -0,0 +1,22 @@
+require 'test/unit'
+require File.dirname(__FILE__) + '/../lib/json-schema'
+
+class ListOptionTest < Test::Unit::TestCase
+  def test_list_option_reusing_schemas
+    schema_hash = {
+      "$schema" => "http://json-schema.org/draft-04/schema#",
+      "type" => "object",
+      "properties" => { "a" => { "type" => "integer" } }
+    }
+
+    uri = URI.parse('http://example.com/item')
+    schema = JSON::Schema.new(schema_hash, uri)
+    JSON::Validator.add_schema(schema)
+
+    data = {"a" => 1}
+    assert(JSON::Validator.validate(uri.to_s, data))
+
+    data = [{"a" => 1}]
+    assert(JSON::Validator.validate(uri.to_s, data, :list => true))
+  end
+end
