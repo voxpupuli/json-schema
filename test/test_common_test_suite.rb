a6793f29feaa13042402e091fdb2693c68cb283b
Matt Palmer <mpalmer@hezmatt.org>
2014-09-18 18:50:56 +1000

    Run json-schema.org's common test suite

---
+++ test/test_common_test_suite.rb
@@ -0,0 +1,52 @@
+require 'test/unit'
+require File.expand_path('../../lib/json-schema', __FILE__)
+
+class CommonTestSuiteTest < Test::Unit::TestCase
+	TEST_DIR = File.expand_path('../test-suite/tests', __FILE__)
+	
+	# These are test files which we know fail spectacularly, either because
+	# we don't support that functionality or because they require external
+	# dependencies
+	IGNORED_TESTS = [
+		"draft3/disallow.json",
+		"draft3/optional/format.json",
+		"draft3/optional/jsregex.json",
+		"draft3/refRemote.json",
+		"draft4/dependencies.json",
+		"draft4/optional/format.json",
+		"draft4/refRemote.json"
+	]
+
+
+	Dir["#{TEST_DIR}/*"].each do |suite|
+		version = File.basename(suite).to_sym
+		Dir["#{suite}/**/*.json"].each do |tfile|
+			test_list = JSON::Validator.parse(File.read(tfile))
+			rel_file = tfile[TEST_DIR.length+1..-1]
+			
+			next if IGNORED_TESTS.include?(rel_file)
+
+			test_list.each do |test|
+				schema = test["schema"]
+				base_description = test["description"]
+				v = nil
+				
+				test["tests"].each do |t|
+					err_id = "#{rel_file}, '#{base_description}'/'#{t['description']}'"
+					
+					define_method("test_#{err_id}") do
+						assert_nothing_raised("Exception raised running #{err_id}") do
+							v = JSON::Validator.validate(schema,
+																  t["data"],
+																  :validate_schema => true,
+																  :version => version
+																 )
+						end
+					
+						assert !!v == t["valid"], "Common test suite case failed: #{err_id}"
+					end
+				end
+			end
+		end
+	end
+end
