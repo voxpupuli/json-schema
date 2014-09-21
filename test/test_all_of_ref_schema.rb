6b7d39a3b7d01fda42272612d0e5a4a7f0c07a63
Matt Palmer <mpalmer@hezmatt.org>
2014-09-21 11:00:16 +1000

    Another allOf ref test, and rename class

--- test/test_all_of_ref_schema.rb
+++ test/test_all_of_ref_schema.rb
@@ -1,11 +1,18 @@
 require 'test/unit'
 require File.dirname(__FILE__) + '/../lib/json-schema'
 
-class AnyOfRefSchemaTest < Test::Unit::TestCase
-  def test_all_of_ref_schema
+class AllOfRefSchemaTest < Test::Unit::TestCase
+  def test_all_of_ref_schema_fails
     schema = File.join(File.dirname(__FILE__),"schemas/all_of_ref_schema.json")
     data = File.join(File.dirname(__FILE__),"data/all_of_ref_data.json")
     errors = JSON::Validator.fully_validate(schema,data, :errors_as_objects => true)
     assert(!errors.empty?, "should have failed to validate")
+  end
+
+  def test_all_of_ref_schema_succeeds
+    schema = File.join(File.dirname(__FILE__),"schemas/all_of_ref_schema.json")
+    data   = %({"name": 42})
+    errors = JSON::Validator.fully_validate(schema,data, :errors_as_objects => true)
+    assert(errors.empty?, "should have validated")
   end
 end
