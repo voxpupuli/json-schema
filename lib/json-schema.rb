5488f37202d0a8ba9e380ceb70fd3d6caf850bdb
Jonas Peschla <github@peschla.net>
2014-10-07 22:16:49 +0200

    Ensure correct order of requires

--- lib/json-schema.rb
+++ lib/json-schema.rb
@@ -19,6 +19,7 @@ require 'json-schema/util/hash'
 require 'json-schema/util/array_set'
 require 'json-schema/schema'
 require 'json-schema/validator'
-Dir[File.join(File.dirname(__FILE__), "json-schema/attributes/**/*.rb")].each {|file| require file }
+Dir[File.join(File.dirname(__FILE__), "json-schema/attributes/*.rb")].each {|file| require file }
+Dir[File.join(File.dirname(__FILE__), "json-schema/attributes/formats/*.rb")].each {|file| require file }
 Dir[File.join(File.dirname(__FILE__), "json-schema/validators/*.rb")].sort!.each {|file| require file }
 require 'json-schema/uri/file'
