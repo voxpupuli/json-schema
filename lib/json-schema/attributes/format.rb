729449a3464902eeb9f3e66cfa2a98da5999d4c1
Jonas Peschla <github@peschla.net>
2014-10-09 22:39:12 +0200

    Move data validity check into attribute class

--- lib/json-schema/attributes/format.rb
+++ lib/json-schema/attributes/format.rb
@@ -6,7 +6,7 @@ module JSON
     class FormatAttribute < Attribute
 
       def self.validate(current_schema, data, fragments, processor, validator, options = {})
-        if TypeAttribute.data_valid_for_type?(data, current_schema.schema['type'])
+        if self.data_valid_for_type?(data, current_schema.schema['type'])
           validator = validator.formats[current_schema.schema['format'].to_s]
           validator.validate(current_schema, data, fragments, processor, validator, options) unless validator.nil?
         end
