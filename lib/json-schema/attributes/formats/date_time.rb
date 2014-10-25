6a22929423d995df06ba72f21903a580a31e8160
Jonas Peschla <github@peschla.net>
2014-10-25 20:39:45 +0200

    Fix wrong file names

--- lib/json-schema/attributes/formats/date_time.rb
+++ lib/json-schema/attributes/formats/date_time.rb
@@ -2,14 +2,24 @@ require 'json-schema/attribute'
 require 'uri'
 module JSON
   class Schema
-    class DateFormat < FormatAttribute
+    class DateTimeFormat < FormatAttribute
       def self.validate(current_schema, data, fragments, processor, validator, options = {})
+        # Timestamp in restricted ISO-8601 YYYY-MM-DDThh:mm:ssZ with optional decimal fraction of the second
         if data.is_a?(String)
-          error_message = "The property '#{build_fragment(fragments)}' must be a date in the format of YYYY-MM-DD"
-          r = Regexp.new('^\d\d\d\d-\d\d-\d\d$')
-          if (r.match(data))
+          error_message = "The property '#{build_fragment(fragments)}' must be a date/time in the ISO-8601 format of YYYY-MM-DDThh:mm:ssZ or YYYY-MM-DDThh:mm:ss.ssZ"
+          r = Regexp.new('^\d\d\d\d-\d\d-\d\dT(\d\d):(\d\d):(\d\d)([\.,]\d+)?(Z|[+-](\d\d)(:?\d\d)?)?$')
+          if (m = r.match(data))
+            parts = data.split("T")
             begin
-              Date.parse(data)
+              Date.parse(parts[0])
+            rescue Exception
+              validation_error(processor, error_message, fragments, current_schema, self, options[:record_errors])
+              return
+            end
+            begin
+              validation_error(processor, error_message, fragments, current_schema, self, options[:record_errors]) and return if m[1].to_i > 23
+              validation_error(processor, error_message, fragments, current_schema, self, options[:record_errors]) and return if m[2].to_i > 59
+              validation_error(processor, error_message, fragments, current_schema, self, options[:record_errors]) and return if m[3].to_i > 59
             rescue Exception
               validation_error(processor, error_message, fragments, current_schema, self, options[:record_errors])
               return
