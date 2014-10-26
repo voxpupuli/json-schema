c8d026565c51a1bbb869ceaaede730aecbb1da19
Jonas Peschla <github@peschla.net>
2014-10-26 12:46:59 +0100

    And another 1.9.3 compatibility fix

--- lib/json-schema/attributes/formats/ip4.rb
+++ lib/json-schema/attributes/formats/ip4.rb
@@ -8,7 +8,8 @@ module JSON
           error_message = "The property '#{build_fragment(fragments)}' must be a valid IPv4 address"
           begin
             ip = IPAddr.new data
-          rescue IPAddr::InvalidAddressError
+          rescue ArgumentError => e
+            raise e unless e.message == 'invalid address'
           end
           validation_error(processor, error_message, fragments, current_schema, self, options[:record_errors]) unless ip && ip.ipv4?
         end
