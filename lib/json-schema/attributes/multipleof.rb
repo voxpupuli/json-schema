8da78f01409b97625252cf3bbc59ad14efa5910f
Kyle Hargraves <pd@krh.me>
2014-10-27 18:12:33 -0500

    Implement multipleOf atop divisibleBy

--- lib/json-schema/attributes/multipleof.rb
+++ lib/json-schema/attributes/multipleof.rb
@@ -1,16 +1,10 @@
-require 'json-schema/attribute'
+require 'json-schema/attributes/divisibleby'
 
 module JSON
   class Schema
-    class MultipleOfAttribute < Attribute
-      def self.validate(current_schema, data, fragments, processor, validator, options = {})
-        return unless data.is_a?(Numeric)
-        if current_schema.schema['multipleOf'] == 0 ||
-            current_schema.schema['multipleOf'] == 0.0 ||
-            (BigDecimal.new(data.to_s) % BigDecimal.new(current_schema.schema['multipleOf'].to_s)).to_f != 0
-          message = "The property '#{build_fragment(fragments)}' was not divisible by #{current_schema.schema['multipleOf']}"
-          validation_error(processor, message, fragments, current_schema, self, options[:record_errors])
-        end
+    class MultipleOfAttribute < DivisibleByAttribute
+      def self.keyword
+        'multipleOf'
       end
     end
   end
