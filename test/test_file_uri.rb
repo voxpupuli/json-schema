dafdfd6d826aaf1933a3026d52c1900ac9413fa6
Kyle Hargraves <pd@krh.me>
2014-11-01 14:57:19 -0500

    test_file_uri: use minitest + data_fixture_path helper

--- test/test_file_uri.rb
+++ test/test_file_uri.rb
@@ -1,9 +1,8 @@
-require 'test/unit'
-require File.dirname(__FILE__) + '/../lib/json-schema'
+require File.expand_path('../test_helper', __FILE__)
 
-class FileUriTest < Test::Unit::TestCase
+class FileUriTest < Minitest::Test
   def test_file_uri_has_correct_components
-    path = File.absolute_path(File.dirname(__FILE__) + '/data/all_of_ref_data.json')
+    path = data_fixture_path('all_of_ref_data.json')
     uri = URI.parse("file://#{path}#name")
     assert_equal('file', uri.scheme)
     assert_equal('', uri.host)
@@ -13,13 +12,13 @@ class FileUriTest < Test::Unit::TestCase
   end
 
   def test_file_uri_loads_real_file
-    path = File.absolute_path(File.dirname(__FILE__) + '/data/all_of_ref_data.json')
+    path = data_fixture_path('all_of_ref_data.json')
     assert_equal(%Q({\n  "name" : "john"\n}\n), open("file://#{path}").read)
   end
 
   def test_file_uri_fails_for_invalid_file
     path = File.absolute_path(File.dirname(__FILE__) + '/this_should_not_exist')
-    assert_raise(Errno::ENOENT) do
+    assert_raises(Errno::ENOENT) do
       open("file://#{path}").read
     end
   end
