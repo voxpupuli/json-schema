f23f39b92961674c81e39eb7375848a71d66a2e6
Kyle Hargraves <pd@krh.me>
2014-11-02 09:34:26 -0600

    Derive DependenciesV4 from Dependencies

--- lib/json-schema/attributes/dependencies.rb
+++ lib/json-schema/attributes/dependencies.rb
@@ -8,25 +8,36 @@ module JSON
 
         current_schema.schema['dependencies'].each do |property, dependency_value|
           next unless data.has_key?(property.to_s)
+          next unless accept_value?(dependency_value)
 
           case dependency_value
           when String
-            if !data.has_key?(dependency_value.to_s)
-              message = "The property '#{build_fragment(fragments)}' has a property '#{property}' that depends on a missing property '#{dependency_value}'"
-              validation_error(processor, message, fragments, current_schema, self, options[:record_errors])
-            end
+            validate_dependency(current_schema, data, property, dependency_value, fragments, processor, self, options)
           when Array
             dependency_value.each do |value|
-              if !data.has_key?(value.to_s)
-                message = "The property '#{build_fragment(fragments)}' has a property '#{property}' that depends on a missing property '#{value}'"
-                validation_error(processor, message, fragments, current_schema, self, options[:record_errors])
-              end
+              validate_dependency(current_schema, data, property, value, fragments, processor, self, options)
             end
           else
             schema = JSON::Schema.new(dependency_value, current_schema.uri, validator)
             schema.validate(data, fragments, processor, options)
           end
         end
+      end
+
+      def self.validate_dependency(schema, data, property, value, fragments, processor, attribute, options)
+        return if data.key?(value.to_s)
+        message = "The property '#{build_fragment(fragments)}' has a property '#{property}' that depends on a missing property '#{value}'"
+        validation_error(processor, message, fragments, schema, attribute, options[:record_errors])
+      end
+
+      def self.accept_value?(value)
+        value.is_a?(String) || value.is_a?(Array) || value.is_a?(Hash)
+      end
+    end
+
+    class DependenciesV4Attribute < DependenciesAttribute
+      def self.accept_value?(value)
+        value.is_a?(Array) || value.is_a?(Hash)
       end
     end
   end
