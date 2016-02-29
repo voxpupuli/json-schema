359f232463ba2709053867336e26a378579b5592
Roberta Reusch-Lewis <roberta@rigor.com>
2016-02-29 18:07:11 -0500

    default to iso8601 if rfc3339 is not supported

--- lib/json-schema/attributes/formats/date_time_v4.rb
+++ lib/json-schema/attributes/formats/date_time_v4.rb
@@ -5,7 +5,11 @@ module JSON
     class DateTimeV4Format < FormatAttribute
       def self.validate(current_schema, data, fragments, processor, validator, options = {})
         return unless data.is_a?(String)
-        DateTime.rfc3339(data)
+        begin
+          DateTime.rfc3339(data)
+        rescue NoMethodError
+          Time.iso8601(data)
+        end
       rescue ArgumentError
         error_message = "The property '#{build_fragment(fragments)}' must be a valid RFC3339 date/time string"
         validation_error(processor, error_message, fragments, current_schema, self, options[:record_errors])
