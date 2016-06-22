03332fcf86bccb707dc7b67f17ab7b2c1cad9e0e
Iain Beeston <iain.beeston@gmail.com>
2016-06-22 14:54:59 +0100

    Moved test-specific classes out of the global namespace
    To avoid unintended side-effects

--- test/extended_schema_test.rb
+++ test/extended_schema_test.rb
@@ -1,28 +1,28 @@
 require File.expand_path('../support/test_helper', __FILE__)
 
-class BitwiseAndAttribute < JSON::Schema::Attribute
-  def self.validate(current_schema, data, fragments, processor, validator, options = {})
-    return unless data.is_a?(Integer)
+class ExtendedSchemaTest < Minitest::Test
+  class BitwiseAndAttribute < JSON::Schema::Attribute
+    def self.validate(current_schema, data, fragments, processor, validator, options = {})
+      return unless data.is_a?(Integer)
 
-    if data & current_schema.schema['bitwise-and'].to_i == 0
-      message = "The property '#{build_fragment(fragments)}' did not evaluate to true when bitwise-AND'd with #{current_schema.schema['bitwise-and']}"
-      validation_error(processor, message, fragments, current_schema, self, options[:record_errors])
+      if data & current_schema.schema['bitwise-and'].to_i == 0
+        message = "The property '#{build_fragment(fragments)}' did not evaluate to true when bitwise-AND'd with #{current_schema.schema['bitwise-and']}"
+        validation_error(processor, message, fragments, current_schema, self, options[:record_errors])
+      end
     end
   end
-end
 
-class ExtendedSchema < JSON::Schema::Validator
-  def initialize
-    super
-    extend_schema_definition("http://json-schema.org/draft-03/schema#")
-    @attributes["bitwise-and"] = BitwiseAndAttribute
-    @uri = Addressable::URI.parse("http://test.com/test.json")
+  class ExtendedSchema < JSON::Schema::Validator
+    def initialize
+      super
+      extend_schema_definition("http://json-schema.org/draft-03/schema#")
+      @attributes["bitwise-and"] = BitwiseAndAttribute
+      @uri = Addressable::URI.parse("http://test.com/test.json")
+    end
+
+    JSON::Validator.register_validator(self.new)
   end
 
-  JSON::Validator.register_validator(self.new)
-end
-
-class ExtendedSchemaTest < Minitest::Test
   def test_extended_schema_validation
     schema = {
       "$schema" => "http://test.com/test.json",
