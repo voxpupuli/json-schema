7b8d2318f9bfb30517558f831caf36947b0a5d27
Iain Beeston <iainbeeston@users.noreply.github.com>
2017-01-31 10:14:32 +0000

    Addressable 2.5.0 changes how file uris are parsed
    Also added a uri util test for a file uri with an explicit host

--- test/uri_util_test.rb
+++ test/uri_util_test.rb
@@ -40,14 +40,24 @@ class UriUtilTest < Minitest::Test
   def test_normalized_uri_for_absolute_path
     str = "/foo/bar.json"
     uri = Addressable::URI.new(scheme: 'file',
-                               path: '///foo/bar.json')
+                               host: '',
+                               path: '/foo/bar.json')
     assert_equal uri, JSON::Util::URI.normalized_uri(str, '/home')
   end
 
-  def test_normalized_uri_for_relartive_path
+  def test_normalized_uri_for_relative_path
     str = "foo/bar.json"
     uri = Addressable::URI.new(scheme: 'file',
-                               path: '///home/foo/bar.json')
+                               host: '',
+                               path: '/home/foo/bar.json')
+    assert_equal uri, JSON::Util::URI.normalized_uri(str, '/home')
+  end
+
+  def test_normalized_uri_for_file_path_with_host
+    str = "file://localhost/foo/bar.json"
+    uri = Addressable::URI.new(scheme: 'file',
+                               host: 'localhost',
+                               path: '/foo/bar.json')
     assert_equal uri, JSON::Util::URI.normalized_uri(str, '/home')
   end
 
