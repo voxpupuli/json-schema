5db8355edb8986fb0264a4aa3e980ed937f43633
Jonne Haß <me@jhass.eu>
2020-02-02 12:38:36 +0100

    Always return duplicates from JSON::Util::URI.parse
    Some callsides seem to modify the returned URI instance, messing up
    the cache. This can lead to the wrong schema fragment being used
    when multiple schemas with overlapping fragment paths are loaded
    and validated against.

--- lib/json-schema/util/uri.rb
+++ lib/json-schema/util/uri.rb
@@ -65,12 +65,8 @@ module JSON
           return uri.dup
         else
           @parse_cache ||= {}
-          parsed_uri = @parse_cache[uri]
-          if parsed_uri
-            parsed_uri.dup
-          else
-            @parse_cache[uri] = Addressable::URI.parse(uri)
-          end
+          @parse_cache[uri] ||= Addressable::URI.parse(uri)
+          @parse_cache[uri].dup
         end
       rescue Addressable::URI::InvalidURIError => e
         raise JSON::Schema::UriError.new(e.message)
