7dbe2630d7e0ca7cfcdbfa10bfbd8c8a2af7da94
Tim Meusel <tim@bastelfreak.de>
2023-02-17 11:56:44 +0100

    rubocop: Fix Style/ClassAndModuleChildren

--- test/support/test_helper.rb
+++ test/support/test_helper.rb
@@ -34,37 +34,39 @@ Dir[File.join(File.expand_path('../', __FILE__), '*.rb')].each do |support_file|
   require support_file unless support_file == __FILE__
 end
 
-class Minitest::Test
-  def suppress_warnings
-    old_verbose = $VERBOSE
-    $VERBOSE = nil
-    begin
-      yield
-    ensure
-      $VERBOSE = old_verbose
+module Minitest
+  class Test
+    def suppress_warnings
+      old_verbose = $VERBOSE
+      $VERBOSE = nil
+      begin
+        yield
+      ensure
+        $VERBOSE = old_verbose
+      end
     end
-  end
 
-  def schema_fixture_path(filename)
-    File.join(File.dirname(__FILE__), '../schemas', filename)
-  end
+    def schema_fixture_path(filename)
+      File.join(File.dirname(__FILE__), '../schemas', filename)
+    end
 
-  def data_fixture_path(filename)
-    File.join(File.dirname(__FILE__), '../data', filename)
-  end
+    def data_fixture_path(filename)
+      File.join(File.dirname(__FILE__), '../data', filename)
+    end
 
-  def assert_valid(schema, data, options = {}, msg = "#{data.inspect} should be valid for schema:\n#{schema.inspect}")
-    errors = validation_errors(schema, data, options)
-    assert_equal([], errors, msg)
-  end
+    def assert_valid(schema, data, options = {}, msg = "#{data.inspect} should be valid for schema:\n#{schema.inspect}")
+      errors = validation_errors(schema, data, options)
+      assert_equal([], errors, msg)
+    end
 
-  def refute_valid(schema, data, options = {}, msg = "#{data.inspect} should be invalid for schema:\n#{schema.inspect}")
-    errors = validation_errors(schema, data, options)
-    refute_equal([], errors, msg)
-  end
+    def refute_valid(schema, data, options = {}, msg = "#{data.inspect} should be invalid for schema:\n#{schema.inspect}")
+      errors = validation_errors(schema, data, options)
+      refute_equal([], errors, msg)
+    end
 
-  def validation_errors(schema, data, options)
-    options = { clear_cache: true, validate_schema: true }.merge(options)
-    JSON::Validator.fully_validate(schema, data, options)
+    def validation_errors(schema, data, options)
+      options = { clear_cache: true, validate_schema: true }.merge(options)
+      JSON::Validator.fully_validate(schema, data, options)
+    end
   end
 end
