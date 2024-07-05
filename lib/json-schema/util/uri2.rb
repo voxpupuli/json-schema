9dd082299fb7f1ab5063c60e6c5af43860f7a080
Tema Bolshakov <either.free@gmail.com>
2024-07-05 22:42:14 +0200

    Replace URI.unescaped_path with URI.unescape_path

--- lib/json-schema/util/uri2.rb
+++ lib/json-schema/util/uri2.rb
@@ -5,6 +5,20 @@ require 'addressable'
 module JSON
   module Util
     class URI2 < Addressable::URI
+      class << self
+        # @param uri [String, Addressable::URI
+        # @return [String]
+        def unescape_path(uri)
+          parse(uri).unescape_path
+        end
+      end
+
+      # Unencodes any percent encoded characters within a path component.
+      #
+      # @return [String]
+      def unescape_path
+        self.class.unescape_component(path)
+      end
     end
   end
 end
