178292e14b617252ee1e21c2803441d5dd53e2a5
Aaron Rosen <aaron.rosen@gusto.com>
2025-05-16 16:50:28 -0600

    failing test

---
+++ test/no_additional_properties_all_of_test.rb
@@ -0,0 +1,41 @@
+require File.expand_path('../support/test_helper', __FILE__)
+
+class NoAdditionalPropertiesAllOfTest < Minitest::Test
+  def schema
+    {
+      '$schema': 'http://json-schema.org/draft-04/schema#',
+      type: 'object',
+      allOf: [
+        {
+          type: 'object',
+          properties: {
+            a: {
+              type: 'integer',
+            },
+          },
+          required: ['a'],
+        },
+        {
+          type: 'object',
+          properties: {
+            b: {
+              type: 'string',
+            },
+          },
+          required: ['b'],
+        },
+      ],
+    }
+  end
+
+  def data
+    {
+      a: 1,
+      b: 'hello',
+    }
+  end
+
+  def test_all_of_ref_message
+    assert_valid schema, data, { noAdditionalProperties: true }
+  end
+end
