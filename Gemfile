bc30615b54d8923374fecfb89e18c36546ce8ee2
Tim Meusel <tim@bastelfreak.de>
2025-07-08 21:33:49 +0200

    Remove unused code coverage

--- Gemfile
+++ Gemfile
@@ -6,8 +6,3 @@ group :release do
   gem 'faraday-retry', require: false
   gem 'github_changelog_generator', require: false
 end
-
-group :coverage, optional: ENV['COVERAGE'] != 'yes' do
-  gem 'codecov', require: false
-  gem 'simplecov-console', require: false
-end
--- test/support/test_helper.rb
+++ test/support/test_helper.rb
@@ -1,29 +1,5 @@
 # frozen_string_literal: true
 
-begin
-  require 'simplecov'
-  require 'simplecov-console'
-  require 'codecov'
-rescue LoadError
-else
-  SimpleCov.start do
-    track_files 'lib/**/*.rb'
-
-    add_filter '/test'
-
-    enable_coverage :branch
-
-    # do not track vendored files
-    add_filter '/vendor'
-    add_filter '/.vendor'
-  end
-
-  SimpleCov.formatters = [
-    SimpleCov::Formatter::Console,
-    SimpleCov::Formatter::Codecov,
-  ]
-end
-
 require 'minitest/autorun'
 require 'webmock/minitest'
 
